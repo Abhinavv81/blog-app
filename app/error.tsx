4a223808a2b436361b756524e6b5204653319e70
FIRST_NAME LAST_NAME <abhinavthakur1008@gmail.com>
2024-08-04 10:37:01 +0530

    removed some comments

--- app/error.tsx
+++ app/error.tsx
@@ -15,7 +15,6 @@ export default function GlobalError({
     <>
       <div className="  flex flex-col justify-center items-center gap-6 h-screen text-center">
         <div className="relative top-80 z-20 flex flex-col justify-center items-center gap-6 text-center ">
-          {/* <div>{error.message}</div> */}
           <div className="flex gap-[190px] mt-4 ">
             <Link href="/">
               <Button>Go Home</Button>
@@ -27,11 +26,10 @@ export default function GlobalError({
         </div>
         <div className="  relative -top-28  flex justify-center items-center w-1/2 ">
           <img
-            src="https://res.cloudinary.com/duysea99p/image/upload/v1722709768/white_error_agreek.png" 
+            src="https://res.cloudinary.com/duysea99p/image/upload/v1722709768/white_error_agreek.png"
             alt=""
             className="w-[450px] "
           />
-          {/* <Not_Found_page> </Not_Found_page> */}
         </div>
       </div>
     </>
