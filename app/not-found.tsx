2226aa9d5984cd6239a03e63fa465ae70cb95ecd
FIRST_NAME LAST_NAME <abhinavthakur1008@gmail.com>
2024-08-04 13:51:00 +0530

    added sotion and ken  in 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,8 +14,8 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <div className="  flex flex-col justify-center items-center gap-6 h-screen text-center">
-      <div className="relative top-[470px] z-20 flex flex-col justify-center items-center gap-6 text-center  ">
-        <div className="flex gap-[315px] mt-4 ml-1 ">
+      <div className="relative top-[640px] z-20 flex flex-col justify-center items-center gap-6 text-center  ">
+        <div className="flex gap-[315px] mt-4 mr-2 ">
           <Link href="/">
             <Button className="h-9">Go Home</Button>
           </Link>
@@ -24,11 +24,11 @@ const NotFound = () => {
           </Link>
         </div>
       </div>
-      <div className="  relative -top-16  flex justify-center items-center w-1/2 ">
+      <div className="  relative -top-5  flex justify-center items-center  ">
         <img
-          src="https://res.cloudinary.com/duysea99p/image/upload/v1722757572/correctWhiteError.png"
+          src="https://res.cloudinary.com/duysea99p/image/upload/v1722759224/404White.png"
           alt=""
-          className="max-w-[700px] "
+          className="max-w-[950px] "
         />
       </div>
     </div>
