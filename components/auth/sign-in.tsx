2f85eea596f2f5d96a99cae7350367afd2fd8656
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-08-09 22:13:37 +0530

    fixes

--- components/auth/sign-in.tsx
+++ components/auth/sign-in.tsx
@@ -1,12 +1,13 @@
 import { SignInGoogle } from "@/actions/auth";
 import { signIn } from "@/auth";
+import { Button } from "../ui/button";
 
 export function SignIn() {
   return (
-    <form
-      action={SignInGoogle}
-    >
-      <button type="submit">Signin with Google</button>
+    <form action={SignInGoogle}>
+      <Button type="submit" variant={"default"} size={"sm"}>
+        Signin with Google
+      </Button>
     </form>
   );
 }
--- components/auth/signout.tsx
+++ components/auth/signout.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { SignOut as Logout } from "@/actions/auth";
 import { Button } from "../ui/button";
 const SignOut = () => {
-  return <form action={Logout}>
-    <Button type="submit">Sign Out</Button>
-  </form>;
+  return (
+    <form action={Logout}>
+      <Button type="submit" size={"sm"}>
+        Sign Out
+      </Button>
+    </form>
+  );
 };
 
 export default SignOut;
