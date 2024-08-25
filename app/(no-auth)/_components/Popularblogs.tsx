c62e9ff37c97031a52df756633f5c833ae10137c
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-08-25 13:00:32 +0530

    chore: Update import path for Footer component in page.tsx

--- app/(no-auth)/_components/Popularblogs.tsx
+++ app/(no-auth)/_components/Popularblogs.tsx
@@ -1,68 +1,62 @@
-import {
-  BellIcon,
-  CalendarIcon,
-  FileTextIcon,
-  GlobeIcon,
-  InputIcon,
-} from "@radix-ui/react-icons";
+// import {
+//   BellIcon,
+//   CalendarIcon,
+//   FileTextIcon,
+//   GlobeIcon,
+//   InputIcon,
+// } from "lucide-react";
 
-import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
+// import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
 
-const features = [
-  {
-    Icon: FileTextIcon,
-    name: "Save your files",
-    description: "We automatically save your files as you type.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
-  },
-  {
-    Icon: InputIcon,
-    name: "Full text search",
-    description: "Search through all your files in one place.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
-  },
-  {
-    Icon: GlobeIcon,
-    name: "Multilingual",
-    description: "Supports 100+ languages and counting.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
-  },
-  {
-    Icon: CalendarIcon,
-    name: "Calendar",
-    description: "Use the calendar to filter your files by date.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
-  },
-  {
-    Icon: BellIcon,
-    name: "Notifications",
-    description:
-      "Get notified when someone shares a file or mentions you in a comment.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
-  },
-];
+// const features = [
+//   {
+//     Icon: FileTextIcon,
+//     name: "Save your files",
+//     description: "We automatically save your files as you type.",
+//     href: "/",
+//     cta: "Learn more",
+//     background: <img className="absolute -right-20 -top-20 opacity-60" />,
+//     className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
+//   },
+//   {
+//     Icon: InputIcon,
+//     name: "Full text search",
+//     description: "Search through all your files in one place.",
+//     href: "/",
+//     cta: "Learn more",
+//     background: <img className="absolute -right-20 -top-20 opacity-60" />,
+//     className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
+//   },
+//   {
+//     Icon: GlobeIcon,
+//     name: "Multilingual",
+//     description: "Supports 100+ languages and counting.",
+//     href: "/",
+//     cta: "Learn more",
+//     background: <img className="absolute -right-20 -top-20 opacity-60" />,
+//     className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
+//   },
+//   {
+//     Icon: CalendarIcon,
+//     name: "Calendar",
+//     description: "Use the calendar to filter your files by date.",
+//     href: "/",
+//     cta: "Learn more",
+//     background: <img className="absolute -right-20 -top-20 opacity-60" />,
+//     className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
+//   },
+//   {
+//     Icon: BellIcon,
+//     name: "Notifications",
+//     description:
+//       "Get notified when someone shares a file or mentions you in a comment.",
+//     href: "/",
+//     cta: "Learn more",
+//     background: <img className="absolute -right-20 -top-20 opacity-60" />,
+//     className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
+//   },
+// ];
 
 export async function Popularblogs() {
-  return (
-    <BentoGrid className="lg:grid-rows-3">
-      {features.map((feature) => (
-        <BentoCard key={feature.name} {...feature} />
-      ))}
-    </BentoGrid>
-  );
+  return <></>;
 }
