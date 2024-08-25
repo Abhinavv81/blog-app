d8664dc332e53cf4ee285a592e863572283dc773
FIRST_NAME LAST_NAME <abhinavthakur1008@gmail.com>
2024-08-25 11:15:34 +0530

    fix

--- app/api/displaypost/route.ts
+++ app/api/displaypost/route.ts
@@ -1,10 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import {NextResponse } from "next/server";
 import BlogModel from "@/models/blog";
+import { connectDB } from "@/lib/db";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
+  await connectDB();
   const displayBlogs = await BlogModel.find({});
   return NextResponse.json(displayBlogs);
-
 }
 
 
